93756362387f48628a09d7daa5f5eadd23bd8e98
Manuel Lopez <aqnuelmagamer8@gmail.com>
2024-10-29 13:22:25 -0600

    agregue el titulo

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -30,7 +30,7 @@ const { onMenuToggle, toggleDarkMode, isDarkTheme } = useLayout();
                     </g>
                 </svg>
 
-                <span>SAKAI</span>
+                <span>Facultad de negocios</span>
             </router-link>
         </div>
 
